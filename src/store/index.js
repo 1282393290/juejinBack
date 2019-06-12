40915dc14196ebb763a4c4154e6c40816b1237fc
chengmin <656330037@qq.com>
2019-06-12 21:04:17 +0800

    活动页今天写了一个模块

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+//import events from './events/index'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  modules: {
-    
-  }
+  // state:{
+
+  // },
+  // modules: {
+
+  // },
+  // actions:{
+
+  // }
+  // modules:{
+  //   events
+  // }
 })
